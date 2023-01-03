55214973477684a1f3b2915466ab6b03701321f4
kvaswin382 <87608397+kvaswin382@users.noreply.github.com>
2023-01-03 16:50:31 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,44 @@
+(function(){
+  
+  var ffmpeg = require('fluent-ffmpeg')
+  const express = require('express')
+  const app = express()
+  
+  const base64 = (str) => {
+   return Buffer.from(str).toString('base64')
+  }
+  
+  const fileName = () => {
+    let date = Date.now()
+    return base64(date)+'.mp3'
+  }
+  
+  app.get('/mp3/:url', (req,res) => {
+    
+    let url = decodeURIComponent(req.params.url)
+    console.log(url)
+    const mp3_file = fileName()
+    ffmpeg(url)
+    .toFormat('mp3')
+    .saveToFile(mp3_file, (stdout,stderr) => {
+      console.log('Converted')
+    })
+    .on('error', (err) => {
+      console.log(err)
+    })
+    .on('progress', (prog) => {
+      console.log('...'+prog)
+    })
+    .on('end',() => {
+      console.log('Finished')
+    })
+    .run()
+    const mp3_path = 'https://mp3con-ath34859.koyeb.app/'+mp3_file
+    const data = {
+      'ok': true,
+      'mp3': mp3_path
+    }
+    res.json(data)
+  })
+  
+})
